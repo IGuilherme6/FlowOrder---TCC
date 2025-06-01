15b99b480979a4366a42ac1ed90239e0014585be
Guilherme <guilhermecostagodoi@gmai.com>
2025-06-01 17:10:56 -0300

    Testes

--- lib/entidades/ItemCardapio.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ffi';
-
-class ItemCardapio {
-  late String _nome;
-  late String _descricao;
-  late double _preco;
-}
---
+++ test/Pedido_test.dart
@@ -0,0 +1,40 @@
+import 'package:test/test.dart';
+import 'package:tcc/models/Pedido.dart';
+
+import 'package:tcc/models/ItemCardapio.dart';
+
+void main() {
+  group('Pedido', () {
+    late Pedido pedido;
+
+    setUp(() {
+      pedido = Pedido()
+        ..statusAtual = 'Aberto';
+    });
+
+    test('deve conter status válidos definidos', () {
+      expect(Pedido.status, containsAll(['Aberto', 'Em Preparo', 'Pronto']));
+    });
+
+    test('deve definir horário corretamente', () {
+      final agora = DateTime.now();
+      pedido.horario = agora;
+      expect(pedido.horario, equals(agora));
+    });
+
+    test('status inicial deve ser Aberto', () {
+      expect(pedido.statusAtual, equals('Aberto'));
+    });
+
+    test('deve adicionar itens corretamente e calcular total', () {
+      final item1 = ItemCardapio()..preco = 10.0;
+      final item2 = ItemCardapio()..preco = 15.5;
+
+      pedido.adicionarItem(item1);
+      pedido.adicionarItem(item2);
+
+      expect(pedido.itens.length, equals(2));
+      expect(pedido.calcularTotal(), equals(25.5));
+    });
+  });
+}
